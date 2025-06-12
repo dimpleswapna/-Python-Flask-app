050a7f24f5c97c17a333701138ef1a9ca30e2de8
dimpleswapna <swapnasowju2011@gmail.com>
2025-06-12 20:45:00 +0530

    Update variables.tf

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -1,3 +1,21 @@
-variable "region" {
-  default = "ap-south-1"
+variable "aws_region" {
+  type        = string
+  description = "The AWS region to deploy to"
+  default     = "ap-south-1"
+}
+
+variable "cluster_name" {
+  type        = string
+  description = "EKS cluster name"
+  default     = "devops-cluster"
+}
+
+variable "vpc_id" {
+  type        = string
+  description = "VPC ID to use"
+}
+
+variable "subnet_ids" {
+  type        = list(string)
+  description = "List of subnet IDs"
 }
